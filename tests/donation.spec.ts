7ebc92481cd10f534920ba0fb689765c950d8b1a
maksym.bondarchuk <maksym.bondarchuk@pmi.bet>
2023-08-09 19:32:44 +0100

    update donation spec

--- tests/donation.spec.ts
+++ tests/donation.spec.ts
@@ -1,12 +1,19 @@
 import { test, expect } from '@playwright/test';
 import { DonationPage } from '../components/donation/donation.page';
+import { DetailsPage } from '../components/donation/details.page';
 
 test.describe('Making a card donation scenario', () => {
   test('Card donation', async ({ page }) => {
     const donationPage = new DonationPage(page);
+    const detailsPage = new DetailsPage(page);
 
+    // Fill Donation page
     await donationPage.openDonationPage();
-    await donationPage.fillDonationPageInfo();
+    await donationPage.fillDonationData();
+    await donationPage.continueToNextPage();
+
+    // Fill Details page
+    await detailsPage.fillDetailsData();
     await donationPage.continueToNextPage();
     await page.pause();
   });
