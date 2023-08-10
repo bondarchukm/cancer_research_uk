718ee179b25af4f51532bd6bea913cbf3e8faf72
maksym.bondarchuk <maksym.bondarchuk@pmi.bet>
2023-08-10 16:16:20 +0100

    update donation.spec

--- tests/donation.spec.ts
+++ tests/donation.spec.ts
@@ -1,11 +1,15 @@
 import { test, expect } from '@playwright/test';
 import { DonationPage } from '../components/donation/donation.page';
 import { DetailsPage } from '../components/donation/details.page';
+import { PaymentPage } from '../components/donation/payment.page';
+import { ThankYouPage } from '../components/donation/thankYou.page';
 
 test.describe('Making a card donation scenario', () => {
   test('Card donation', async ({ page }) => {
     const donationPage = new DonationPage(page);
     const detailsPage = new DetailsPage(page);
+    const paymentPage = new PaymentPage(page);
+    const thankYouPage = new ThankYouPage(page);
 
     // Fill Donation page
     await donationPage.openDonationPage();
@@ -15,6 +19,21 @@ test.describe('Making a card donation scenario', () => {
     // Fill Details page
     await detailsPage.fillDetailsData();
     await donationPage.continueToNextPage();
-    await page.pause();
+
+    // Fill Payment page
+    await paymentPage.fillPaymentData();
+    await paymentPage.continueToNextPage();
+
+    // Verify donation reference
+    const donationReferenceFromResponce =
+      await thankYouPage.getDonationReferenceFromResponse(page);
+
+    const donationReferenceFromPage =
+      await thankYouPage.getDonationReferenceFromPage();
+
+    expect(
+      donationReferenceFromResponce,
+      'Donation reference from the response is not equal to donation reference from the page',
+    ).toEqual(donationReferenceFromPage);
   });
 });
